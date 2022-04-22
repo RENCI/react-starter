7e8b7c832776278c6405da49afed232054f3e55b
Matt Watson <mwatson@renci.org>
2022-04-22 15:42:41 -0400

    migrate to new react rendering api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { render } from 'react-dom'
 import { App } from './components/app'
-import './styles/index.scss'
+import { createRoot } from 'react-dom/client'
 
-render(<App />, document.getElementById('root'))
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(<App />)
